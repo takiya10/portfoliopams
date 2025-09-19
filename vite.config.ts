6bca6f99223d5e968fca8057ce83dff1c0763dac
takiya10 <takiya.resultz10@gmail.com>
2025-09-19 12:37:09 +0700

    fix all

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfoliopams/',           // <- punyamu
   plugins: [react()],
-  base: '/portfoliopams/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // <- alias '@' -> src
+    },
   },
 });
